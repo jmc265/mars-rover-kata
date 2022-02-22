23b17e3a993c4426da95ef02e9b6881c46386973
James Cross <james@breakingwave.com>
2022-02-22 12:50:10 +0000

    Move tests

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -2,7 +2,8 @@ import {add} from "../index";
 
 describe("add function", () => {
     it.each([
-        [1, 2, 3]
+        [1, 2, 3],
+        [4, 5, 9]
     ])("should add %i & %i to get %i", (a, b, expected) => {
         expect(add(a, b)).toEqual(expected);
     });
